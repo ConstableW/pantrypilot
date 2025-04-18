a5cdb9c1704b56939b4d51c1985582cf6d5b0321
ConstableW <134558059+ConstableW@users.noreply.github.com>
2025-04-18 14:29:11 +0200

    ewr

--- addon/main.py
+++ addon/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/ping")
-def ping():
-    return {"message": "pong"}
+@app.get("/")
+def read_root():
+    return {"message": "PantryPilot Safe is running!"}
--- addon/run.sh
+++ addon/run.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/with-contenv bashio
+#!/usr/bin/env bash
 cd /app
 uvicorn main:app --host 0.0.0.0 --port 8010
